403903511a50a8e7ddea90e556b357f37f0a9681
kel <yamariva_2000@yahoo.com>
2017-02-09 10:22:27 -0800

    Adjusted URL for years before 2011

--- vehicle_miles_traveled.py
+++ vehicle_miles_traveled.py
@@ -4,13 +4,23 @@ import urllib
 
 months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
 years = range(10, 17)
-years = [str(year) for year in years]
 
-address = 'https://www.fhwa.dot.gov/policyinformation/travel_monitoring/{}/{}.xls'
+yearstext = [str(year) for year in years]
+
+addressnew = 'https://www.fhwa.dot.gov/policyinformation/travel_monitoring/{}/{}.xls'
+addressold='https://www.fhwa.dot.gov/ohim/tvtw/{}/{}.xls'
+
 
 for year in years:
     for month in months:
         file = '{}{}tvt'.format(year, month)
+
+        if year>10:
+            address=addressnew
+        else:
+            address=addressold
+
         url = address.format(file, file)
+
         print url
         urllib.urlretrieve(url, './vmt_files/{}.xls'.format(file))

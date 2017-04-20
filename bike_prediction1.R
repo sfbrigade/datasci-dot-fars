637ceff2fc088c8fbe686a0247fc30cca4636a21
Kevin Vo <kevinavo@Kevins-MacBook-Pro.local>
2017-04-19 19:21:42 -0700

    added tablesadded tablesadded tablesadded tablesadded tablesadded tablesadded tablesadded tablesadded tables

---
+++ bike_prediction1.R
@@ -0,0 +1,52 @@
+#Author: Kevin Vo
+#This R script implements an interrupted time series
+#approach to determine if there is a significant change
+#in the number of bike deaths per capita in the year
+#2015.
+
+rm(list = ls())
+
+Preprocess <- function(){
+	setwd("/Users/kevinavo/Repos/fars_project/state-outliers")
+	library(plm)
+	library(data.table)
+	library(DataCombine)
+	library(plyr)
+	library(stargazer)
+}
+
+MakeData <- function(){
+	#Read in and clean the data
+	df = read.csv('data-2.csv')
+	names(df) = tolower(names(df))
+	df = rename(df, c("vmt..b." = 'vmt_b', 'total.deaths' = 'total_deaths', 
+					  'deaths.per.bvmt' = 'deaths_bvmt', 'deaths.involving.drivers.under.16' = 'deaths_16', 
+					  'deaths.involving.drivers.under.16.per.bvmt' = 'deaths_16_bvmt', 
+					  'deaths.involving.drivers.between.25...44' = 'deaths_25_44', 
+					  'deaths.involving.drivers.between.25...44.per.bvmt' = 'deaths_25_44_bvmt', 
+					  'multi.vehicle.deaths'= 'mult_vehicle_deaths', 
+					  'multi.vehicle.deaths.per.bvmt' = 'multi_vehicle_deaths_per_bvmt', 
+					  'bicyclist.deaths' = 'bike_deaths', 'bicyclist.deaths.per.bvmt' = 'bike_deaths_bvmt'))
+	
+	# #Generate an indicator variable for pre and post year 2015
+	df['post'] = as.numeric((df['year'] == 2015))
+	df['year'] = df['year'] - 2009	
+	return (df)
+}
+
+##########Main Scripting##########
+Preprocess()
+df = MakeData()
+listOfFits = list()
+depVars = c('deaths_bvmt', 'deaths_16_bvmt', 'deaths_25_44_bvmt', 'multi_vehicle_deaths_per_bvmt', 'bike_deaths_bvmt')
+for (depVar in depVars){
+	regModel = paste(depVar, '~', 'year + post + state', sep = '')
+	print (regModel)
+	regModel = as.formula(regModel)
+	lm_fit = lm(regModel, data = df)
+	listOfFits[[depVar]] = lm_fit
+}
+
+stargazer(listOfFits[1:3])
+stargazer(listOfFits[4:5])
+
